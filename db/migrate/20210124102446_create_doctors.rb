9c828bc76c77d527c2703a3bfa6af96d5a3c3067
iamrivera <a3m3rivera@gmail.com>
2021-01-24 03:07:35 -0800

    Creating the Doctor and Patient migrations with tables

--- db/migrate/20210124102446_create_doctors.rb
+++ db/migrate/20210124102446_create_doctors.rb
@@ -1,7 +1,9 @@
 class CreateDoctors < ActiveRecord::Migration
   def change
     create_table :doctors do |t|
-      
+      t.string :name
+      t.string :email
+      t.string :password
       t.timestamps null: false
     end
   end
